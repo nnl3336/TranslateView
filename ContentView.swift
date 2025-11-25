0e59e1505c07d5ed744b684fe5f9e7248a912ad2
nnl3336 <may.be.be.k12@gmail.com>
2025-11-25 17:30:31 +0900

    commit-o

--- ContentView.swift
+++ ContentView.swift
@@ -30,80 +30,45 @@ import UIKit
 
 import UIKit
 
+import UIKit
+
 class ViewController: UIViewController {
 
-    let greetingLabel = UILabel()
-    let changeLangButton = UIButton(type: .system)
-    
-    // 現在の言語コード
-    var currentLang = "ja" {
-        didSet {
-            updateTexts()
-        }
-    }
-    
-    // 選択中のバンドル
-    private var languageBundle: Bundle = .main
+    let label = UILabel()
+    let button = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .white
-        
-        // ラベル
-        greetingLabel.font = .systemFont(ofSize: 24)
-        greetingLabel.textColor = .black
-        greetingLabel.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(greetingLabel)
-        
-        // ボタン
-        changeLangButton.setTitleColor(.black, for: .normal)
-        changeLangButton.translatesAutoresizingMaskIntoConstraints = false
-        changeLangButton.addTarget(self, action: #selector(showLanguageSheet), for: .touchUpInside)
-        view.addSubview(changeLangButton)
-        
+
+        // ---- Label ----
+        label.text = NSLocalizedString("greeting", comment: "")
+        label.font = .systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        // ---- Button ----
+        button.setTitle(NSLocalizedString("translate", comment: ""), for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 20)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(tapTranslate), for: .touchUpInside)
+        view.addSubview(button)
+
+        // ---- Layout ----
         NSLayoutConstraint.activate([
-            greetingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            greetingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            changeLangButton.topAnchor.constraint(equalTo: greetingLabel.bottomAnchor, constant: 20),
-            changeLangButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50),
+
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 32)
         ])
-        
-        updateTexts()
     }
-    
-    func updateTexts() {
-        // 言語用バンドルを切替
-        if let path = Bundle.main.path(forResource: currentLang, ofType: "lproj"),
-           let bundle = Bundle(path: path) {
-            languageBundle = bundle
-        } else {
-            languageBundle = .main
-        }
-        
-        // 文字列を再取得して更新
-        greetingLabel.text = NSLocalizedString("greeting", bundle: languageBundle, comment: "")
-        changeLangButton.setTitle(NSLocalizedString("changeLang", bundle: languageBundle, comment: ""), for: .normal)
-        navigationItem.title = NSLocalizedString("editMemo", bundle: languageBundle, comment: "")
-    }
-    
-    @objc func showLanguageSheet() {
-        let alert = UIAlertController(title: NSLocalizedString("selectLanguage", bundle: languageBundle, comment: ""), message: nil, preferredStyle: .actionSheet)
-        
-        let languages: [(String, String)] = [
-            ("ja", "日本語"),
-            ("en", "English"),
-            ("zh-Hans", "中文（简体）"),
-            ("zh-Hant", "中文（繁體）")
-        ]
-        
-        for (code, name) in languages {
-            alert.addAction(UIAlertAction(title: name, style: .default) { _ in
-                self.currentLang = code
-            })
-        }
-        
-        alert.addAction(UIAlertAction(title: NSLocalizedString("cancelButton", bundle: languageBundle, comment: ""), style: .cancel))
-        present(alert, animated: true)
+
+    @objc func tapTranslate() {
+        // 押したら常に最新の翻訳で更新
+        label.text = NSLocalizedString("hello", comment: "")
+        button.setTitle(NSLocalizedString("translate", comment: ""), for: .normal)
     }
 }
 
@@ -122,7 +87,7 @@ struct ViewControllerWrapper: UIViewControllerRepresentable {
 struct ContentView: View {
     var body: some View {
         ViewControllerWrapper()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white) // 白背景を確実に
+            .ignoresSafeArea()   // ← これ重要！
+            .background(Color.white)
     }
 }
